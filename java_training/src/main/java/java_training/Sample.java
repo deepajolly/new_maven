869295b0585262a34d8e24acbd85504a5b3faa7d
deepajolly <63454512+deepajolly@users.noreply.github.com>
2020-04-14 05:04:32 -0700

    uploading my project to repository

---
+++ java_training/src/main/java/java_training/Sample.java
@@ -0,0 +1,14 @@
+package java_training;
+
+public class Sample {
+	
+		public static void main(String args[]) 
+		{
+			int a=10;
+			int b=50;
+			int sum=a+b;
+			System.out.print("Sum is "+sum);
+		}
+		}
+
+
